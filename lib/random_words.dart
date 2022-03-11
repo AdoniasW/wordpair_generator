4f928d461817cf692fedb1f7582cfd80f210bb37
AdoniasW <adoniaswondwosen123@gmail.com>
2022-03-11 06:06:20 -0800

    last commit

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -8,7 +8,7 @@ class RandomWords extends StatefulWidget{
 
 class RandomWordsState extends State<RandomWords>{
   final _randomWordPairs =<WordPair>[];
-
+   final _savedWordPairs=Set<WordPair>();
   Widget _buildList(){
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
@@ -23,14 +23,53 @@ class RandomWordsState extends State<RandomWords>{
     );
   }
   Widget _buildRow(WordPair pair){
+    final alreadySaved =_savedWordPairs.contains(pair);
     return ListTile(title: Text(pair.asPascalCase,style:
     TextStyle(fontSize: 18)
-    ));
+    ),
+  trailing:Icon(alreadySaved ? Icons.favorite: Icons.favorite_border,
+      color : alreadySaved ? Colors.red : null) ,
+      onTap: () {
+      setState((){
+        if(alreadySaved){
+          _savedWordPairs.remove(pair);
+        }
+        else{
+          _savedWordPairs.add(pair);
+        }
+      });
+      }
+    );
 
+  }
+  void _pushSaved (){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+          builder: (BuildContext context) {
+            final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+              return ListTile(
+                  title: Text(
+                      pair.asPascalCase, style: TextStyle(fontSize: 16.0))
+              );
+            });
+            final List<Widget> divided=ListTile.divideTiles(
+              context: context,
+              tiles:tiles).toList();
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Usernames')
+              ),
+              body:ListView(children:divided));
+
+          }));
   }
   Widget build(BuildContext context){
     return Scaffold(
-        appBar: AppBar(title: Text('WordPair Generator'),),
+        appBar: AppBar(title: Text('Username Generator'),
+          actions: <Widget>[
+            IconButton(onPressed:_pushSaved, icon: Icon(Icons.list))
+          ],
+        ),
         body:_buildList()
     );
   }

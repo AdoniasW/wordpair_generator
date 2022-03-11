11ca761429ebb2c9bdb3e0e0b886d4dd26de7b71
AdoniasW <adoniaswondwosen123@gmail.com>
2022-03-11 06:17:33 -0800

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.purple),
+
+      theme: ThemeData(primaryColor: Colors.black38),
         home: RandomWords());
   }
 }
